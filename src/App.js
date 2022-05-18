67060f3e190251c394bd8586f5a2c26103fd66f1
Mohammad Rabbani <mohammad.rabbani@tigeranalytics.local>
2022-05-18 13:05:51 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { SendPost } from "./components/SendPost/SendPost.jsx";
-import { UpdateAccountInfo } from "./components/UpdateAccountInfo/UpdateAccountInfo.jsx";
-import { Comments } from "./components/Comments/Comments.jsx";
-import { ViewPosts } from "./components/ViewPosts/ViewPosts.jsx";
+import SendPost from "./components/SendPost/SendPost.jsx";
+import UpdateAccountInfo from "./components/UpdateAccountInfo/UpdateAccountInfo.jsx";
+import Comments from "./components/Comments/Comments.jsx";
+import ViewPosts from "./components/ViewPosts/ViewPosts.jsx";
 
 // header myprofile viewpost footer
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="header" element={<Comments />}></Route>
+        <Route exact path="/" element={<Comments />}></Route>
         <Route exact path="myprofile" element={<SendPost />}></Route>
         <Route exact path="viewpost" element={<ViewPosts />}></Route>
         <Route exact path="footer" element={<UpdateAccountInfo />}></Route>
