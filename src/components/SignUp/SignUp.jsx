57ad3d5b9b50d9a4b9da1ee8cba571d19e2f2f3c
Mohammad Rabbani <mohammad.rabbani@tigeranalytics.local>
2022-05-19 10:50:09 +0530

    updated

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,29 +3,42 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-export default function SignUp() {
-  return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="First Name" variant="outlined" />
-      <br />
-      <TextField id="filled-basic" label="Last Name" variant="filled" />
-      <br />
-      <TextField id="filled-basic" label="Email" variant="filled" />
-      <br />
-      <TextField id="filled-basic" label="Password" variant="filled" />
-      <br />
-      <TextField id="filled-basic" label="Re Enter Password" variant="filled" />
-      <br />
-      <Stack spacing={2} direction="row">
-        <Button variant="text">SignUp</Button>
-      </Stack>
-    </Box>
-  );
+import { render } from "@testing-library/react";
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="First Name" variant="outlined" />
+        <br />
+        <TextField id="filled-basic" label="Last Name" variant="filled" />
+        <br />
+        <TextField id="filled-basic" label="Email" variant="filled" />
+        <br />
+        <TextField id="filled-basic" label="Password" variant="filled" />
+        <br />
+        <TextField
+          id="filled-basic"
+          label="Re Enter Password"
+          variant="filled"
+        />
+        <br />
+        <Stack spacing={2} direction="row">
+          <Button variant="text">SignUp</Button>
+        </Stack>
+      </Box>
+    );
+  }
 }
+
+export default SignUp;
